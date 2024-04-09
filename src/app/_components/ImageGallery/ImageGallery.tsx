b5e335c507d4c17a66e7d1da6db3cd96b7722a37
Shin Kim <shin.kim8245@gmail.com>
2024-04-09 09:48:30 -0400

    built initial layout

--- src/app/_components/ImageGallery/ImageGallery.tsx
+++ src/app/_components/ImageGallery/ImageGallery.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+/* eslint-disable @next/next/no-img-element */
+import React, { useState } from "react";
 
-const ImageGallery = () => {
-  return <div>ImageGallery</div>;
+type ImageGalleryProps = {
+  imageUrls: string[];
+};
+
+const ImageGallery: React.FC<ImageGalleryProps> = ({ imageUrls }) => {
+  const validImageUrls = Array.isArray(imageUrls) ? imageUrls : [];
+
+  const [selectedImage, setSelectedImage] = useState<string>(
+    validImageUrls[0] ?? "defaultImageUrl",
+  );
+
+  return (
+    <div className="flex h-[669px] w-[728px]">
+      <div className="ml-2 mt-12 flex flex-col space-y-2.5 pl-12">
+        {validImageUrls.map((url, index) => (
+          <img
+            key={index}
+            src={url}
+            alt={`Thumbnail ${index + 1}`}
+            className="h-[60px] w-[60px] cursor-pointer"
+            onClick={() => setSelectedImage(url)}
+          />
+        ))}
+      </div>
+
+      <div className="flex-grow">
+        <img src={selectedImage} alt="Selected" className="h-full w-full" />
+      </div>
+    </div>
+  );
 };
 
 export { ImageGallery };
