5d823b7fa95d16a78ad9def8b9f5099c80ed52a8
Babur Dilshat <baburdilshat@gmail.com>
2024-04-10 14:02:14 -0700

    deleted unnesscary code

--- src/app/_components/SearchBar/DesktopSearchBar.tsx
+++ src/app/_components/SearchBar/DesktopSearchBar.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
-import { Modal } from "../Modal";
 
 const DesktopSearchBar = () => {
   const onSubmit = () => {
@@ -21,20 +20,6 @@ const DesktopSearchBar = () => {
       >
         <MagnifyingGlassIcon className="ml-2 h-4 w-4 cursor-pointer" />
       </button>
-      {/* <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="-mt-9 ml-1 mr-1 hidden size-0 h-5 w-5 md:flex"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-        />
-      </svg> */}
     </div>
   );
 };
