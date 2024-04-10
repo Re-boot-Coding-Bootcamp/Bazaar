68eb870353fd756d50b6182c107dc7d8b4f90838
Muradil Erkin <miller.muradil@gmail.com>
2024-04-10 19:25:28 -0400

    minor styling change

--- src/app/_components/CartItem/CartItem.tsx
+++ src/app/_components/CartItem/CartItem.tsx
@@ -1,3 +1,4 @@
+import { XMarkIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 
 /* eslint-disable @next/next/no-img-element */
@@ -27,84 +28,76 @@ const CartItem: React.FC<{ product: Product }> = ({ product }) => {
 
   const totalPrice = price * quantity;
 
+  const handleQuantityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.target.value);
+    if (value <= 10 && value >= 1) {
+      setQuantity(value);
+    }
+  };
+
   return (
     <div
       id="cart-item-container"
-      className="mx-auto max-w-3xl overflow-hidden rounded bg-white shadow-md"
+      className="w-full rounded bg-white shadow-md md:flex"
     >
-      <div
-        id="item-name-container"
-        className="justify-items-start border-b border-gray-200 pb-1 md:flex"
-      >
-        <div className="md:shrink-0">
-          <img
-            src={image}
-            alt="image description"
-            className="h-48 w-full object-cover pr-4 md:h-full md:w-48"
-          ></img>
-        </div>
-        <div className="flex flex-col justify-start pr-10 pt-1">
-          <a href="" className="mb-2 text-2xl font-black">
-            {name}
-          </a>
-          <div className="flex flex-col gap-1">
-            <text className="text-sm font-thin text-slate-500">{color}</text>
-            <text className="mb-10 text-sm font-thin text-slate-500">
-              {size}
-            </text>
-            <div className="justify-content-between flex items-center">
-              <button
-                type="button"
-                className="round border border-black bg-gray-200 px-3 py-1 hover:bg-gray-300 focus:outline-none"
-                onClick={decrementQty}
-              >
-                -
-              </button>
-              <input
-                type="number"
-                min={1}
-                max={10}
-                className="border border-y-black py-1 text-center"
-                value={quantity}
-              ></input>
-              <button
-                type="button"
-                className="round border border-black bg-gray-200 px-3 py-1 hover:bg-gray-300 focus:outline-none"
-                onClick={incrementQty}
-              >
-                +
-              </button>
-            </div>
+      <div id="image-container" className="md:shrink-0">
+        <img
+          src={image}
+          alt="image description"
+          className="aspect-square w-full object-cover md:w-48"
+        />
+      </div>
+      <div id="content-container" className="flex w-full justify-between p-4">
+        <div
+          id="content-left-container"
+          className="flex flex-col justify-between gap-4"
+        >
+          <div>
+            <a href="" className="mb-2 text-2xl font-black">
+              {name}
+            </a>
+            <p className="text-sm font-thin text-slate-500">{color}</p>
+            <p className="text-sm font-thin text-slate-500">{size}</p>
+          </div>
+          <div className="flex items-center">
+            <button
+              type="button"
+              className="round border border-black bg-gray-200 px-3 py-1 hover:bg-gray-300 focus:outline-none"
+              onClick={decrementQty}
+            >
+              -
+            </button>
+            <input
+              type="text"
+              min={1}
+              max={10}
+              className="max-w-8 border border-y-black py-1 text-center"
+              value={quantity}
+              onChange={handleQuantityChange}
+            />
+            <button
+              type="button"
+              className="round border border-black bg-gray-200 px-3 py-1 hover:bg-gray-300 focus:outline-none"
+              onClick={incrementQty}
+            >
+              +
+            </button>
           </div>
         </div>
-        <div className="flex flex-col justify-between pl-10 pr-1 pt-1">
-          <text className="py-1 pb-1 text-2xl font-black">
+        <div
+          id="content-right-container"
+          className="flex flex-col items-end justify-between"
+        >
+          <p className="text-2xl font-black">
             {totalPrice.toLocaleString("en-US", {
               style: "currency",
               currency: "USD",
             })}
-          </text>
-          <div className="flex flex-row">
-            <button>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="h-6 w-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M6 18 18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-            <a href="" className="mb-1 underline">
-              Remove item
-            </a>
-          </div>
+          </p>
+          <button className="flex underline hover:text-red-500">
+            <XMarkIcon className="h-6 w-6" />
+            Remove item
+          </button>
         </div>
       </div>
     </div>
