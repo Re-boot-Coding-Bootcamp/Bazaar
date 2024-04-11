8b404dbfcdb2b12d7f2cacab3f0424050063d972
Muradil Erkin <miller.muradil@gmail.com>
2024-04-11 19:26:39 -0400

    beautiful footer

--- src/app/_components/Footer/Footer.stories.tsx
+++ src/app/_components/Footer/Footer.stories.tsx
@@ -2,19 +2,22 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Footer } from "./Footer";
 
 const meta = {
-    title: "Components/Footer",
-    component: Footer,
-    parameters: {
-      layout: "centered",
-    },
-    tags: ["autodocs"],
-    argTypes: {},
-  } satisfies Meta<typeof Footer>;
-  
-  export default meta;
-  type Story = StoryObj<typeof meta>;
-  
-  export const Primary: Story = {
-    args: {},
-  };
-  
+  title: "Components/Footer",
+  component: Footer,
+  tags: ["autodocs"],
+  argTypes: {},
+  decorators: [
+    (Story) => (
+      <div className="w-full">
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof Footer>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {},
+};
--- src/app/_components/Footer/Footer.tsx
+++ src/app/_components/Footer/Footer.tsx
@@ -1,37 +1,18 @@
 import React from "react";
 
+const currentYear = new Date().getFullYear();
+
 const Footer = () => {
   return (
-    <>
-      <footer className="m-4 rounded-lg bg-white shadow dark:bg-gray-800">
-        <div className="mx-auto w-full max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2023{" "}
-            <a href="https://vans.com/" className="hover:underline">
-              VANS
-            </a>
-            . All Rights Reserved.
-          </span>
-          <ul className="mt-3 flex flex-wrap items-center text-sm font-medium text-gray-500 sm:mt-0 dark:text-gray-400">
-            <li>
-              <a href="#" className="me-4 hover:underline md:me-6">
-                About Us
-              </a>
-            </li>
-            <li>
-              <a href="#" className="me-4 hover:underline md:me-6">
-                Privacy Policy
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Contact Us
-              </a>
-            </li>
-          </ul>
-        </div>
-      </footer>
-    </>
+    <footer className="flex w-full justify-center">
+      <span className="text-sm text-gray-500 sm:text-center">
+        © {currentYear}{" "}
+        <a href="https://vans.com/" className="hover:underline">
+          Bazaar
+        </a>
+        . All Rights Reserved.
+      </span>
+    </footer>
   );
 };
 
