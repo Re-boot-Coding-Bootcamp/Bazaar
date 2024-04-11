2e460c1513a1a65784b90510d558356de615ee3a
Shin Kim <shin.kim8245@gmail.com>
2024-04-11 13:01:04 -0400

    finished

--- src/app/_components/BreadCrumb/BreadCrumb.stories.tsx
+++ src/app/_components/BreadCrumb/BreadCrumb.stories.tsx
@@ -1,15 +1,26 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { BreadCrumb } from "./BreadCrumb";
 
-const meta: Meta = {
-  title: "Component/BreadCrumb",
+const meta: Meta<typeof BreadCrumb> = {
+  title: "Components/BreadCrumb",
   component: BreadCrumb,
   tags: ["autodocs"],
+  argTypes: {
+    items: {
+      control: "object",
+    },
+  },
 };
+
 export default meta;
 
-type Story = StoryObj<typeof meta>;
-
-export const Default: Story = {
-  args: {},
+export const Default: StoryObj<typeof BreadCrumb> = {
+  args: {
+    items: [
+      { name: "Home", href: "/home" },
+      { name: "Kids", href: "/home/kids" },
+      { name: "Shoes", href: "/home/kids/shoes" },
+      { name: "Boy", current: true },
+    ],
+  },
 };
--- src/app/_components/BreadCrumb/BreadCrumb.tsx
+++ src/app/_components/BreadCrumb/BreadCrumb.tsx
@@ -1,41 +1,41 @@
 import React from "react";
 import { ChevronRightIcon } from "@heroicons/react/20/solid";
 
-const BreadCrumb = () => {
-  return (
-    <nav className="flex" aria-label="Breadcrumb">
-      <ol className="inline-flex items-center space-x-1 md:space-x-2">
-        <li className="inline-flex items-center">
+interface BreadCrumbProps {
+  items: Array<{
+    name: string;
+    href?: string;
+    current?: boolean;
+    onClick?: () => void;
+  }>;
+}
+
+const BreadCrumb = ({ items }: BreadCrumbProps) => (
+  <nav aria-label="Breadcrumb">
+    <ol className="flex flex-wrap items-center space-x-1 md:space-x-3">
+      {items.map((item, index) => (
+        <li key={`${item.name}-${index}`} className="flex items-center">
+          {index > 0 && (
+            <ChevronRightIcon className="mx-1 h-3 w-3 text-gray-400 md:mx-3" />
+          )}
           <a
-            href="#"
-            className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-cyan-600"
+            href={item.current ? undefined : item.href ?? "#"}
+            onClick={(e) => {
+              if (item.href === undefined) {
+                e.preventDefault();
+                item.onClick && item.onClick();
+              }
+            }}
+            className={`text-sm font-medium ${item.current ? "text-cyan-600" : "text-gray-700"}`}
+            aria-current={item.current ? "page" : undefined}
+            aria-label={!item.current ? `Go to ${item.name}` : undefined}
           >
-            <ChevronRightIcon className="h-4 w-4 text-gray-800  " />
-            Home
+            {item.name}
           </a>
         </li>
-        <li>
-          <div className="flex items-center">
-            <ChevronRightIcon className="h-4 w-4 text-gray-800  " />
-            <a
-              href="#"
-              className="ms-1 text-sm font-medium text-gray-700 hover:text-cyan-600 "
-            >
-              Projects
-            </a>
-          </div>
-        </li>
-        <li aria-current="page">
-          <div className="flex items-center">
-            <ChevronRightIcon className="h-4 w-4 text-gray-800  " />
-            <span className="ms-1 text-sm font-medium text-cyan-600">
-              Flowbite
-            </span>
-          </div>
-        </li>
-      </ol>
-    </nav>
-  );
-};
+      ))}
+    </ol>
+  </nav>
+);
 
 export { BreadCrumb };
