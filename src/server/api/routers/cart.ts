9c66678da6c7f7f0cc866a66602360318407bba4
Jack Vu <jacknvu98@gmail.com>
2024-04-18 07:56:38 +0900

    worked on this one two don't know if it is right

--- src/server/api/routers/cart.ts
+++ src/server/api/routers/cart.ts
@@ -8,4 +8,27 @@ export const cartRouter = createTRPCRouter({
       data: {},
     });
   }),
+  updateProductQuantityInCart: publicProcedure
+    .input(
+      z.object({
+        cartId: z.string(),
+        cartItemId: z.string(),
+        quantity: z.number(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      if (input.quantity > 10) {
+        throw new Error("Quantity added cannot be over 10");
+      }
+      if (input.quantity > 0 && input.quantity <= 10) {
+        await ctx.db.cartItem.update({
+          where: { id: input.cartItemId },
+          data: { quantity: input.quantity },
+        });
+      }
+      if (input.quantity === 0) {
+        await ctx.db.cartItem.delete({ where: { id: input.cartItemId } });
+      }
+      return await ctx.db.cart.findUnique({ where: { id: input.cartId } });
+    }),
 });
