c427c66ee87215a2c471be9181e3a2ce6b3cd2a7
Jack Vu <jacknvu98@gmail.com>
2024-04-18 07:20:09 +0900

    idk looks okay don't know if it works but looks pretty

--- src/server/api/routers/cart.ts
+++ src/server/api/routers/cart.ts
@@ -8,4 +8,33 @@ export const cartRouter = createTRPCRouter({
       data: {},
     });
   }),
+
+  addProductToCart: publicProcedure
+    .input(z.object({ productVariantId: z.string(), cartId: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const productToAddToCart = await ctx.db.productVariant.findUnique({
+        where: { id: input.productVariantId },
+        include: { stock: true },
+      });
+
+      if (!productToAddToCart) {
+        throw new Error("Product variant not found");
+      }
+
+      if (productToAddToCart.stock <= 0) {
+        throw new Error("Product out of stock");
+      }
+
+      await ctx.db.cartItem.create({
+        data: {
+          cartId: input.cartId,
+          productVariantId: input.productVariantId,
+          quantity: 1,
+        },
+      });
+
+      return await ctx.db.cart.findUnique({
+        where: { id: input.cartId },
+      });
+    }),
 });
