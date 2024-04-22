e6e8f49d9d89fb44ccb141809f78354ac8cbfad2
Muradil Erkin <miller.muradil@gmail.com>
2024-04-22 18:27:50 -0400

    also simplified it a bit

--- src/server/api/routers/product.ts
+++ src/server/api/routers/product.ts
@@ -1,23 +1,10 @@
-import { TRPCError } from "@trpc/server";
-import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const productRouter = createTRPCRouter({
-  getTopSeller: publicProcedure.query(async ({ ctx }) => {
-    const allProductVariantsAndQuantitySold = await ctx.db.topSeller.findMany({
+  getTopSeller: publicProcedure.query(({ ctx }) => {
+    return ctx.db.topSeller.findMany({
       orderBy: { quantitySold: "desc" },
+      take: 4,
     });
-    if (!allProductVariantsAndQuantitySold) {
-      throw new TRPCError({
-        message: "Top Sellers can't be found",
-        code: "INTERNAL_SERVER_ERROR",
-      });
-    }
-    // this is before I did the findMany orderBy wanted to know if the orderBy works the way I think it will
-    const sortedProductVariantsDescendingOrder =
-      allProductVariantsAndQuantitySold.sort((a, b) => {
-        b.quantitySold - a.quantitySold;
-      });
-    return sortedProductVariantsDescendingOrder.slice(0, 4);
   }),
 });
