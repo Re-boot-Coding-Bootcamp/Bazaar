6a6154a955d7863d7c8863a5b031e312c2647878
Shin Kim <shin.kim8245@gmail.com>
2024-04-22 17:22:27 -0400

    finished but need to figure out how to handle decimal

--- src/server/api/routers/product.ts
+++ src/server/api/routers/product.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const productRouter = createTRPCRouter({
@@ -7,4 +8,39 @@ export const productRouter = createTRPCRouter({
       take: 4,
     });
   }),
+  getProducts: publicProcedure
+    .input(
+      z.object({
+        categoryId: z.string().optional(),
+        size: z.string().optional(),
+        color: z.string().optional(),
+        price: z
+          .object({
+            min: z.number().optional(),
+            max: z.number().optional(),
+          })
+          .optional(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const productFilters = {
+        ...(input.categoryId && { categoryId: input.categoryId }),
+        ...(input.size && { size: input.size }),
+        ...(input.color && { color: input.color }),
+        ...(input.price && {
+          price: {
+            gte: input.price.min,
+            lte: input.price.max,
+          },
+        }),
+      };
+
+      const applyFilters = Object.keys(productFilters).length > 0;
+
+      const products = await ctx.db.product.findMany({
+        where: applyFilters ? productFilters : {},
+      });
+
+      return products;
+    }),
 });
