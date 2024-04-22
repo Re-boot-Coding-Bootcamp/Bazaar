11858803030b77af391a3fa7ed45af5f884b53d5
Jack Vu <jacknvu98@gmail.com>
2024-04-22 17:09:57 -0400

    keep running into an unsafe assigment of any when I try to findMany

--- src/server/api/routers/product.ts
+++ src/server/api/routers/product.ts
@@ -7,14 +7,14 @@ export const productRouter = createTRPCRouter({
     const allProductVariantsAndQuantitySold = await ctx.db.topSeller
       .findMany({
         orderBy: { quantitySold: "desc" },
-      })
-      .slice(0, 4);
+      });
     if (!allProductVariantsAndQuantitySold) {
       throw new TRPCError({
         message: "Top Sellers can't be found",
         code: "INTERNAL_SERVER_ERROR",
       });
     }
+    // this is before I did the findMany orderBy wanted to know if the orderBy works the way I think it will
     const sortedProductVariantsDescendingOrder =
       allProductVariantsAndQuantitySold.sort((a, b) => {
         b.quantitySold - a.quantitySold;
