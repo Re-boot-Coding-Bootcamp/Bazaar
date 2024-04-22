8eb3cfbbbf944f6f5cb8966a9dc126782c333a8b
Jack Vu <jacknvu98@gmail.com>
2024-04-22 17:08:35 -0400

    help me lol

--- src/server/api/routers/product.ts
+++ src/server/api/routers/product.ts
@@ -1,5 +1,24 @@
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-import { createTRPCRouter } from "~/server/api/trpc";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-export const productRouter = createTRPCRouter({});
+export const productRouter = createTRPCRouter({
+  getTopSeller: publicProcedure.query(async ({ ctx }) => {
+    const allProductVariantsAndQuantitySold = await ctx.db.topSeller
+      .findMany({
+        orderBy: { quantitySold: "desc" },
+      })
+      .slice(0, 4);
+    if (!allProductVariantsAndQuantitySold) {
+      throw new TRPCError({
+        message: "Top Sellers can't be found",
+        code: "INTERNAL_SERVER_ERROR",
+      });
+    }
+    const sortedProductVariantsDescendingOrder =
+      allProductVariantsAndQuantitySold.sort((a, b) => {
+        b.quantitySold - a.quantitySold;
+      });
+    return sortedProductVariantsDescendingOrder.slice(0, 4);
+  }),
+});
