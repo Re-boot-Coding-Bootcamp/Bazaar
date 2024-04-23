38e29b12ba2ad6f373efe449f09dbab9b606310d
Jack Vu <jacknvu98@gmail.com>
2024-04-23 17:43:26 -0400

    tote bag, water bottle, polo, hoodies, puffer jackets done

--- src/server/api/routers/data.ts
+++ src/server/api/routers/data.ts
@@ -1,9 +1,13 @@
+import { TRPCError } from "@trpc/server";
+import { url } from "inspector";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-// const colors = ["Black", "White", "Gray", "Blue"];
-// const sizes = ["XS", "S", "M", "L", "XL", "2XL", "3XL"];
+const colors = ["Black", "White", "Gray", "Blue"];
+const colors2 = ["Green", "Black", "Light Gray", "Dark Gray"];
+const colors3 = ["Black", "Light Gray", "Dark Gray"];
+const sizes = ["XS", "S", "M", "L", "XL", "2XL", "3XL"];
 // const colorAndImageUrl: Record<string, string> = {
 //   Black:
 //     "https://murad-public-files.s3.amazonaws.com/bazaar/product-images/4-simple-t-shirt-black.png",
@@ -12,6 +16,42 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 //   Gray: "https://murad-public-files.s3.amazonaws.com/bazaar/product-images/6-simple-t-shirt-gray.png",
 //   Blue: "https://murad-public-files.s3.amazonaws.com/bazaar/product-images/7-simple-t-shirt-blue.png",
 // };
+const colorAndImageUrlWaterBottles: Record<string, string> = {
+  Black:
+    "https://murad-public-files.s3.amazonaws.com/bazaar/product-images/11-water-bottle-black.png",
+  White:
+    "https://murad-public-files.s3.amazonaws.com/bazaar/product-images/13-water-bottle-white.png",
+  Gray: "https://murad-public-files.s3.amazonaws.com/bazaar/product-images/14-water-bottle-gray.png",
+  Blue: "https://murad-public-files.s3.amazonaws.com/bazaar/product-images/12-water-bottle-blue.png",
+};
+const colorAndImageUrlPolos: Record<string, string> = {
+  Green:
+    "https://murad-public-files.s3.amazonaws.com/bazaar/product-images/15-polo-green.png",
+  Black:
+    "https://murad-public-files.s3.amazonaws.com/bazaar/product-images/16-polo-black.png",
+  "Light Gray":
+    "https://murad-public-files.s3.amazonaws.com/bazaar/product-images/17-polo-light-gray.png",
+  "Dark Gray":
+    "https://murad-public-files.s3.amazonaws.com/bazaar/product-images/18-polo-dark-gray.png",
+};
+const colorAndImageUrlHoodies: Record<string, string> = {
+  Green:
+    "https://murad-public-files.s3.amazonaws.com/bazaar/product-images/20-hoodie-green.png",
+  Black:
+    "https://murad-public-files.s3.amazonaws.com/bazaar/product-images/21-hoodie-black.png",
+  "Light Gray":
+    "https://murad-public-files.s3.amazonaws.com/bazaar/product-images/22-hoodie-light-gray.png",
+  "Dark Gray":
+    "https://murad-public-files.s3.amazonaws.com/bazaar/product-images/22-hoodie-charcoal.png",
+};
+const colorAndImageUrlPuffer: Record<string, string> = {
+  Black:
+    "https://murad-public-files.s3.amazonaws.com/bazaar/product-images/23-puffer-jacket-black.png",
+  "Light Gray":
+    "https://murad-public-files.s3.amazonaws.com/bazaar/product-images/25-puffer-jacket-light-gray.png",
+  "Dark Gray":
+    "https://murad-public-files.s3.amazonaws.com/bazaar/product-images/24-puffer-jacket-anthracite.png",
+};
 
 export const dataRouter = createTRPCRouter({
   creator: publicProcedure.query(async ({ ctx }) => {
@@ -50,5 +90,139 @@ export const dataRouter = createTRPCRouter({
     // return ctx.db.image.createMany({
     //   data: imageData,
     // });
+
+    // create variant for tote bag using product ID clvcrgsgl00009lgjm5kpuysy
+    // price $28.99 size OSFA color white stock 99
+
+    await ctx.db.productVariant.create({
+      data: {
+        productId: "clvcrgsgl00009lgjm5kpuysy",
+        price: 28.99,
+        color: "White",
+        size: "OSFA",
+        stock: 99,
+      },
+    });
+
+    //create Image for tote bag variant image: https://murad-public-files.s3.amazonaws.com/bazaar/product-images/8-cotton-tote-bag.png
+    const toteVariant = await ctx.db.productVariant.findUnique({
+      where: { id: "clvcrgsgl00009lgjm5kpuysy" },
+    });
+    await ctx.db.image.create({
+      data: {
+        productVariantId: toteVariant?.id ?? "",
+        url: "https://murad-public-files.s3.amazonaws.com/bazaar/product-images/8-cotton-tote-bag.png",
+      },
+    });
+
+    // create water bottle product variants productId clvcvox7k00019lgj6x35w8y1
+    const waterBottleProductVariantsData = colors.map((color) => {
+      return {
+        productId: "clvcvox7k00019lgj6x35w8y1",
+        price: 19.99,
+        stock: 99,
+        color,
+        size: "L", // no idea about water bottle size
+      };
+    });
+    await ctx.db.productVariant.createMany({
+      data: waterBottleProductVariantsData,
+    });
+    // grab all variants
+    const waterBottleVariants = await ctx.db.productVariant.findMany({
+      where: { productId: "clvcvox7k00019lgj6x35w8y1" },
+    });
+    // add create image for all water bottle variants
+    const waterBottleImageData = waterBottleVariants.map((variant) => {
+      return {
+        productVariantId: variant.id,
+        url: colorAndImageUrlWaterBottles[variant.color] ?? "",
+      };
+    });
+    await ctx.db.image.createMany({ data: waterBottleImageData });
+
+    // polo variants productId: clvcvt7d600029lgjhymeqbdy
+    const poloProductVariantData = colors2.flatMap((color) => {
+      return sizes.map((size) => {
+        return {
+          productId: "clvcvt7d600029lgjhymeqbdy",
+          price: 42.99,
+          stock: 99,
+          color,
+          size,
+        };
+      });
+    });
+    await ctx.db.productVariant.createMany({
+      data: poloProductVariantData,
+    });
+
+    const poloVariants = await ctx.db.productVariant.findMany({
+      where: { productId: "clvcvt7d600029lgjhymeqbdy" },
+    });
+
+    const imagePoloData = poloVariants.map((variant) => {
+      return {
+        productVariantId: variant.id,
+        url: colorAndImageUrlPolos[variant.color] ?? "",
+      };
+    });
+    await ctx.db.image.createMany({ data: imagePoloData });
+
+    // hoodies productId: clvcwfhts00039lgjgiqki0hf
+    const hoodiesProductVariantData = colors2.flatMap((color) => {
+      return sizes.map((size) => {
+        return {
+          productId: "clvcwfhts00039lgjgiqki0hf",
+          price: 52.99,
+          stock: 99,
+          color,
+          size,
+        };
+      });
+    });
+    await ctx.db.productVariant.createMany({
+      data: hoodiesProductVariantData,
+    });
+
+    const hoodieVariants = await ctx.db.productVariant.findMany({
+      where: { productId: "clvcwfhts00039lgjgiqki0hf" },
+    });
+
+    const hoodieVariantsData = hoodieVariants.map((variant) => {
+      return {
+        productVariantId: variant.id,
+        url: colorAndImageUrlHoodies[variant.color] ?? "",
+      };
+    });
+    await ctx.db.image.createMany({ data: hoodieVariantsData });
+
+    // puffer jacket productId: clvcwrttr00049lgjpeqfevze
+    const pufferProductVariantData = colors3.flatMap((color) => {
+      return sizes.map((size) => {
+        return {
+          productId: "clvcwrttr00049lgjpeqfevze",
+          price: 62.99,
+          stock: 99,
+          color,
+          size,
+        };
+      });
+    });
+    await ctx.db.productVariant.createMany({
+      data: pufferProductVariantData,
+    });
+
+    const pufferVariants = await ctx.db.productVariant.findMany({
+      where: { productId: "clvcwrttr00049lgjpeqfevze" },
+    });
+
+    const pufferVariantsData = pufferVariants.map((variant) => {
+      return {
+        productVariantId: variant.id,
+        url: colorAndImageUrlPuffer[variant.color] ?? "",
+      };
+    });
+    await ctx.db.image.createMany({ data: pufferVariantsData });
   }),
 });
